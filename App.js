9fd0edaa5aa93e535c8c1202823829812bc4bb2f
hernandez87v <hernandez87v@hotmail.com>
2020-12-01 15:53:12 -0800

    try and get vector icons to show in Icon.Button

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import { Button, StyleSheet, View } from 'react-native'
 import Header from './components/Header'
+import Icon from 'react-native-vector-icons/FontAwesome';
+// const myButton = (<Icon.Button name="instagram" size={30} color="#900">Check My Instagram!</Icon.Button>);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header title="1st React Native App" />
-      <Button
-        onPress={() => {
-          alert('You tapped the button!');
-        }}
-        title="Press Me"
-      />
+      <View style={styles.buttonContainer}>
+        <Button
+          style={styles.buttonLayout}
+          onPress={() => {
+            alert('You tapped the button!');
+          }}
+          title="Press Me"
+        />
+        <Icon.Button name="instagram" backgroundColor="#3b5998">
+          Check My Instagram!
+        </Icon.Button>
+      </View>
     </View>
   );
 }
@@ -19,6 +27,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black'
+    backgroundColor: 'black',
+  },
+  buttonContainer: {
+    margin: 40,
+  },
+  buttonLayout: {
+    borderRadius: 100/2,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
