10a20366ba6f6429fd7cbc7e6cab5e7b9dc479c6
hernandez87v <hernandez87v@hotmail.com>
2020-12-01 14:49:45 -0800

    add button to see notification behaviour

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, View } from 'react-native'
 import Header from './components/Header'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Header title='1st React Native App'/>
-      </View>
+      <Header title="1st React Native App" />
+      <Button
+        onPress={() => {
+          alert('You tapped the button!');
+        }}
+        title="Press Me"
+      />
+    </View>
   );
 }
 
